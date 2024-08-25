592a33203b30922d6162dd6e1ae5ac0f3911dccb
Inigo <iblanco-@student.42urduliz.com>
2024-08-25 19:04:53 +0200

    ex05 done

---
+++ Module 01/ex05/Harl.hpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+class Harl {
+	private:
+		void debug(void);
+		void info(void);
+		void warning(void);
+		void error(void);
+		
+	public:
+		void complain(std::string level);
+};
+
+
+#endif
