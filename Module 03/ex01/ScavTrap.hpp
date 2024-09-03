1fa8d4d78a5873766c4a77820a21340ae2dce4f8
iblanco-s <iblanco-@student.42urduliz.com>
2024-09-03 18:06:55 +0200

    recovery

---
+++ Module 03/ex01/ScavTrap.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScavTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iblanco- <iblanco-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/03 16:50:38 by iblanco-          #+#    #+#             */
+/*   Updated: 2024/09/03 16:50:39 by iblanco-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCAVTRAP_HPP
+# define SCAVTRAP_HPP
+
+#include <iostream>
+#include "ClapTrap.hpp"
+
+class ScavTrap : virtual public ClapTrap {
+	public:
+		ScavTrap();
+		ScavTrap(std::string name);
+		ScavTrap(const ScavTrap& other);
+		ScavTrap& operator=(const ScavTrap& other);
+		~ScavTrap();
+
+		void attack(const std::string& target);
+		void guardGate();
+};
+
+#endif
